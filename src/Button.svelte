c6288bd2205952f5ce3e3d1d41d6f3ffc8e33736
Veeru <veeru.neerukonda@gmail.com>
2022-07-13 19:22:44 +0530

    feat: recieve feedback text form user and allow submit
    A input was added to the FeedbackForm component so that the user can enter feedback which needs to be displayed
    
    A button is appended to the FeedbackForm component which can be used to submit said feedback.
    
    Added few utility scss classes which are expected to be self evident.

---
+++ src/Button.svelte
@@ -0,0 +1,40 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+    let dispatcher = createEventDispatcher();
+
+    export let text;
+    export let disabled = true;
+
+    function handleClick(){
+        dispatcher('btn:click');
+    }
+</script>
+
+<button class="btn" {disabled} on:click|preventDefault={handleClick}>{text}</button>
+
+<style lang="scss">
+    button {
+        color: #fff;
+        border: 0;
+        border-radius: 8px;
+        background-color: #202142;
+
+        width: 100px;
+        height: 40px;
+        cursor: pointer;
+    }
+    
+    button:hover {
+        transform: scale(0.98);
+        opacity: 0.9;
+    }
+    button:disabled{
+        background-color: #cccccc;
+        color: #333;
+        cursor:auto;
+    }
+    button:disabled:hover{
+        transform:scale(1);
+        opacity: 1;
+    }
+</style>
--- src/FeedbackForm.svelte
+++ src/FeedbackForm.svelte
@@ -1,18 +1,36 @@
 <script>
     import Card from './Card.svelte';
+    import Button from './Button.svelte';
     import RatingBox from './RatingBox.svelte';
+
+    let feedbackText = '';
 </script>
 
 <div class="wrapper">
     <Card class="card--full-width">
         <form class="feedback-form__form" action="submit">
             <h1 class="feedback-form__heading-primary">How would you rate your service with us?</h1>
-            <RatingBox />
+            <div class="feedback-form__input-section">
+                <div class="u-margin-y-big u-width-95 u-center-x">
+                    <RatingBox />
+                </div>
+                <div class="feedback-form__input-group">
+                    <input class="feedback-form__input" type="text" bind:value={feedbackText}>
+                    <div class="u-margin-x-small">
+                        <Button text="Send" disabled={feedbackText.length <= 10} on:btn:click={ ()=>{console.log('button clicked!')}}/>
+                    </div>
+                </div>
+                {#if feedbackText.length <= 10}
+                    <h2 class="feedback-form__error-heading">Text must be atleast 10 characters long</h2>
+                {/if}
+            </div>
         </form>
     </Card>
 </div>
 
 <style lang="scss">
+    @import './scss/variables';
+    @import './scss/utilities';
     .wrapper{
         margin: 8rem 0;
     }
@@ -20,7 +38,42 @@
     .feedback-form{
         &__heading-primary{
             font-size: 2.8rem;
-            text-transform: capitalize;
+            font-weight: 600;
+            width: 26ch;
+            margin: 0 auto;
+        }
+
+        &__input-section{
+            margin-top: 4rem;
+        }
+
+        &__input-group{
+            position: relative;
+            display: flex;
+            align-items: center;
+
+            border-radius: 5px;
+            border: 1px solid $color-dark-1;
+        }
+
+        &__input{
+            flex: 1 1 100%;
+            font-size: 1.8rem;
+            padding: 1.4rem 1rem;
+
+            border-radius: 5px;
+            border: 0px solid transparent;
+
+            &:focus{
+                outline: 0px solid transparent;
+            }
+        }
+
+        &__error-heading{
+            padding-top: 10px;
+            text-align: center;
+            color: rebeccapurple;
+            font-weight: 400;
         }
     }
 </style>
